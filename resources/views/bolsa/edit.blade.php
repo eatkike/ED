27144dc4f531e47cd832651096da5d2307872209
janaquintana2004-crypto <janaquintana2004@gmail.com>
2026-03-18 15:38:53 -0600

    editar

---
+++ resources/views/bolsa/edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EDITAR BOLSA DE AHORRO</title>
+</head>
+<body>
+    <h1>EDITAR BOLSA DE AHORRO DE: {{$usuario->nombre}}</h1>
+
+    
+</body>
+</html>
