e8f8be96664693c594a6a531bc36b4f8505adfb0
Joel <ffjo230544@upemor.edu.mx>
2026-03-18 20:28:13 -0600

    Avances

--- app/Models/Bolsa.php
+++ app/Models/Bolsa.php
@@ -6,20 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bolsa extends Model
 {
-<<<<<<< HEAD
-=======
   protected $table = 'bolsas';
 
->>>>>>> a845a6dfbd23d2d1a6d5f6c6fceea893517ad4f3
     protected $fillable = [
         'Nombre',
         'Meta',
         'Fecha de Meta',
         'Descripcion',
-<<<<<<< HEAD
-        'Monto Actual',
-=======
         'Monto Actual'
->>>>>>> a845a6dfbd23d2d1a6d5f6c6fceea893517ad4f3
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -6,12 +6,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
-//Actualizar
-//Route::put('/bolsa/{id}',[
-    //BolsaController::class, 'update'
-//])->name('bolsas.update');
-=======
 Route::resource('bolsas', BolsaController::class);
 Route::get('/bolsas/{id}/edit', [BolsaController::class, 'edit'])->name('bolsas.edit');
 Route::put('/bolsas/{id}', [BolsaController::class, 'update'])->name('bolsas.update');
@@ -19,4 +13,3 @@ Route::delete('/bolsas/{id}', [BolsaController::class, 'destroy'])->name('bolsas
 Route::get('/bolsas', [BolsaController::class, 'index'])->name('bolsas.index');
 
     
->>>>>>> a4ab0f35a3d992ebc2c3b6c79495f0f73c8599dd
